a3da655f59053d7b94d6e21470fc3968be222c38
Max Kowalski <13492750+maximuskowalski@users.noreply.github.com>
2020-09-22 19:03:07 +1000

    Create wanchors.sh
    Create Watch Anchors.

---
+++ wanchors.sh
@@ -0,0 +1,8 @@
+  
+#!/bin/bash
+# A script to watch for rclone mount anchors and stop muh dockers.
+
+# REQUIRED VARIABLES
+ANCHOR=td_tv1.bin # user name goes here
+SAPPS=plex,emby,jellyfin # docker service apps
+NOTIFICATION=
