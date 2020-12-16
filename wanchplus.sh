b60877f814073fe8f490950ef21225d446ecfc3e
Max Kowalski <13492750+maximuskowalski@users.noreply.github.com>
2020-12-16 18:06:00 +1100

    Update wanchplus.sh
    Fix quotations

--- wanchplus.sh
+++ wanchplus.sh
@@ -5,7 +5,7 @@
 ANCHOR=audiobooks,td_tv2.bin                          # anchors, use commas (a directory is also a file)
 DIR="/mnt/unionfs"                                    # location of anchor files
 SAPPS="plex emby emby2 plexhex jellyfin calibre"      # docker service apps, separate with spaces.
-SYSAPPS="mergerfs"                                    # system services, separate with spaces.
+SYSAPPS="mergerfs munter"                             # system services, separate with spaces.
 WANCHLOG="/home/max/logs/wanch.log"                   # logfile
 
 checker() {
@@ -18,7 +18,7 @@ thrillkillkult() {
   docker stop ${SAPPS}
   sudo systemctl restart ${SYSAPPS}
   docker start ${SAPPS}
-  echo 'Time: `$(date)`. mount dropped, apps restarted.' >> '${WANCHLOG}'
+  echo "Time: "$(date)". mount dropped, "${SYSAPPS}" restarted." >> ${WANCHLOG}
 }
 
 checker
