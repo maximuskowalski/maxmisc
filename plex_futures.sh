d63949f3eb84d63049e3df2b01598587aa3c81d9
Max Kowalski <matthewbarger@gmail.com>
2023-01-11 15:47:37 +1100

    Update to use plex sequel
    sequelo squeelo

--- plex_futures.sh
+++ plex_futures.sh
@@ -1,13 +1,23 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 PLEXDOCKER=plex
-PLEXDBPATH="/opt/plex/app/Plex Media Server/Plug-in Support/Databases/"
+PLEXDBPATH="/opt/plex/Library/Application Support/Plex Media Server/Plug-in Support/Databases/"
+PLEXDB="com.plexapp.plugins.library.db"
+PLEXSQL="/opt/plexsql/Plex Media Server"
+
+TODAY=$(date '+%Y_%d_%m__%H_%M_%S')
 
 docker stop "${PLEXDOCKER}"
-cp "${PLEXDBPATH}com.plexapp.plugins.library.db" "${PLEXDBPATH}com.plexapp.plugins.library.db.bak"
-sqlite3 "${PLEXDBPATH}com.plexapp.plugins.library.db" <<END_SQL
+cd "${PLEXDBPATH}" || return
+
+cp "${PLEXDB}" "${PLEXDB}_${TODAY}.bak"
+
+([ -e "com.plexapp.plugins.library.db-shm" ] && rm com.plexapp.plugins.library.db-shm)
+([ -e "rm com.plexapp.plugins.library.db-wal" ] && rm rm com.plexapp.plugins.library.db-wal)
+
+"${PLEXSQL}" --sqlite "${PLEXDB}" <<END_SQL
 .timeout 2000
-UPDATE metadata_items SET added_at = DATETIME('now') WHERE DATETIME(added_at) > DATETIME('now');;;
+UPDATE metadata_items SET added_at = DATETIME('now') WHERE DATETIME(added_at) > DATETIME('now');
 END_SQL
 docker start "${PLEXDOCKER}"
 #eof
--- plex_futures_n_stuckers.sh
+++ plex_futures_n_stuckers.sh
@@ -12,11 +12,9 @@ cd "${PLEXDBPATH}" || return
 
 cp "${PLEXDB}" "${PLEXDB}_${TODAY}.bak"
 
-([ -e "com.plexapp.plugins.library.db-shm" ] && rm com.plexapp.plugins.library.db-shm)
-([ -e "rm com.plexapp.plugins.library.db-wal" ] && rm rm com.plexapp.plugins.library.db-wal)
-
 "${PLEXSQL}" --sqlite "${PLEXDB}" <<END_SQL
 .timeout 2000
+UPDATE metadata_items SET added_at = DATETIME('now') WHERE DATETIME(added_at) > DATETIME('now');
 UPDATE metadata_items SET added_at = originally_available_at WHERE added_at <> originally_available_at AND originally_available_at IS NOT NULL;
 END_SQL
 docker start "${PLEXDOCKER}"
