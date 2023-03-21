e9c9a36ccade4c67abd33fe8d14e06ca332c90c1
Max Kowalski <matthewbarger@gmail.com>
2023-03-21 11:49:55 +1100

    atrain test and code revision

--- atrain.sh
+++ atrain.sh
@@ -1,44 +1,47 @@
 #!/usr/bin/env bash
 # https://github.com/maximuskowalski/maxmisc/blob/master/atrain.sh
-# an A-Train installer
+# An A-Train installer
 
-set -Eeuo pipefail
+# shellcheck disable=SC2154
+
+#________ DEV
+# set -x
+# set -Eeuo pipefail
+
 IFS=$'\n\t'
 
 #________ VARS
-
+# shellcheck source=/dev/null
 source "$(dirname "$0")/maxmisc.conf"
-
-# atrainname=atrain
-# appdir=/opt
-# network=saltbox
-
-#________ DONT CHANGE
-
-# atrainimagetag=latest
-# atrainmntpnt=${appdir}/${atrainname}
-# atrainconfig="${atrainmntpnt}/a-train.toml"
 
 #________ FUNCTIONS
 
-checkoff() {
-  ([ -d "${atrainmntpnt}" ] || dirmkr)
-  ([ -e ${atrainconfig} ] || configo)
+# Check for root runners
+check_root() {
+  if [ "$(whoami)" = root ]; then
+    printf "Running as root or with sudo is not supported. Exiting.\n"
+    exit
+  fi
 }
 
-dirmkr() {
-  sudo mkdir -p "${atrainmntpnt}" && sudo chown "${USER}":"${USER}" "${atrainmntpnt}"
+# Check if directories and files exist
+check_resources() {
+  ([ -d "${atrainmntpnt}" ] || create_directory)
+  ([ -e "${atrainconfig}" ] || create_config)
 }
 
-# let user complete - no var replacing
-# use heredoc instead of var
+# Create directory if it doesn't exist
+create_directory() {
+  sudo mkdir -p "${atrainmntpnt}" && sudo chown "${myusername}":"${myusername}" "${atrainmntpnt}" && sudo chmod 775 "${atrainmntpnt}"
+}
 
-configo() {
+# Create configuration file if it doesn't exist
+create_config() {
   cat >"${atrainconfig}" <<EOF
 # a-train.toml
 [autoscan]
 # Replace the URL with your Autoscan URL.
-url = "http://localhost:3030"
+url = "http://autoscan:3030"
 username = "hello there"
 password = "general kenobi"
 
@@ -51,11 +54,13 @@ drives = ["0A1xxxxxxxxxUk9PVA", "0A2xxxxxxxxxUk9PVA"]
 EOF
 }
 
-tugger() {
-  docker image pull ghcr.io/m-rots/a-train:${atrainimagetag}
+# Pull A-Train Docker image
+pull_docker_image() {
+  docker image pull ghcr.io/m-rots/a-train:"${atrainimagetag}"
 }
 
-dockery() {
+# Run A-Train Docker container
+run_docker_container() {
   docker run -d \
     --name "${atrainname}" \
     --volume "${atrainmntpnt}":/data \
@@ -66,45 +71,26 @@ dockery() {
     ghcr.io/m-rots/a-train
 }
 
-dockstop() {
+# Stop A-Train Docker container
+stop_docker_container() {
   docker stop "${atrainname}"
-  echo "...A-Train docker has been stopped for configuration"
+  printf "...A-Train docker has been stopped for configuration\n"
 }
 
-messaging() {
-  echo
-  echo "    You must be running the cloudb0x/autoscan:bernard-rs docker image"
-  echo "    for A-Train events to be accepted as triggers"
-  echo
-  echo "    A-Train initial documentation:"
-  echo "    https://gist.github.com/m-rots/f345fd2cfc44585266b620feb9fbd612"
-  echo
-  echo "    A-Train Autoscan documentation:"
-  echo "    https://github.com/Cloudbox/autoscan/tree/bernard-rs#a-train"
-  echo
-  echo "    your configuration is files is located here"
-  echo "    ${atrainmntpnt}/a-train.toml"
-  echo "    remember to add a service account"
-  echo
-  echo "    to start A-Train after configuration is completed..."
-  echo "    docker start ${atrainname}"
-}
-
-fin() {
-  echo
-  echo "    **************************"
-  echo "    * ---------------------- *"
-  echo "    * - install completed! - *"
-  echo "    * ---------------------- *"
-  echo "    **************************"
-  echo
+# Display link information and messages
+show_messages() {
+  printf "\n    A-Train documentation:\n    https://github.com/m-rots/a-train\n\n"
+  printf "    Autoscan documentation:\n    https://github.com/Cloudbox/autoscan\n\n"
+  printf "    Your configuration file is located here:\n    %s/a-train.toml\n" "${atrainmntpnt}"
+  printf "    Remember to add a service account\n\n"
+  printf "    To start A-Train after configuration is completed...\n    docker start %s\n" "${atrainname}"
 }
 
 #________ RUNLIST
 
-checkoff
-tugger
-dockery
-dockstop
-messaging
-fin
+check_root
+check_resources
+pull_docker_image
+run_docker_container
+stop_docker_container
+show_messages
