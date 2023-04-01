fa749e69c464863b292df72109084c4ebc3eea23
Max Kowalski <matthewbarger@gmail.com>
2023-04-01 19:39:03 +1100

    plex_futures and stuckers cleanup
    add backup suffix to conf file and sql timeout

--- plex_futures.sh
+++ plex_futures.sh
@@ -1,25 +1,63 @@
 #!/usr/bin/env bash
+# https://github.com/maximuskowalski/maxmisc/blob/master/plex_futures.sh
 
+# This script corrects Plex items that have an incorrect added date in the future.
+
+# shellcheck disable=SC2154
+
+#________ DEV
+# set -x
+# set -Eeuo pipefail
+
+# IFS for safer handling of filenames and paths
+IFS=$'\n\t'
+
+#________ VARIABLES
+# Source the configuration file
+# shellcheck source=/dev/null
 source "$(dirname "$0")/maxmisc.conf"
 
-# plexdockername=plex
-# plexdbpath="/opt/plex/Library/Application Support/Plex Media Server/Plug-in Support/Databases/"
-# plexdb="com.plexapp.plugins.library.db"
-# plexsql="/opt/plexsql/Plex Media Server"
+#________ FUNCTIONS
+# Stop the Plex Docker container
+stop_plex_container() {
+    docker stop "${plexdockername}"
+}
 
-# today=$(date '+%Y_%d_%m__%H_%M_%S')
+# Backup the Plex database
+backup_plex_db() {
+    cd "${plexdbpath}" || return
+    cp "${plexdb}" "${plexdb}_${today}${backup_suffix}"
+}
 
-docker stop "${plexdockername}"
-cd "${plexdbpath}" || return
+# Remove the SQLite journal files from the Plex database directory
+remove_sqlite_journals() {
+    cd "${plexdbpath}" || return
+    rm -f com.plexapp.plugins.library.db-shm
+    rm -f com.plexapp.plugins.library.db-wal
+}
 
-cp "${plexdb}" "${plexdb}_${today}.bak"
-
-([ -e "com.plexapp.plugins.library.db-shm" ] && rm com.plexapp.plugins.library.db-shm)
-([ -e "rm com.plexapp.plugins.library.db-wal" ] && rm rm com.plexapp.plugins.library.db-wal)
-
-"${plexsql}" --sqlite "${plexdb}" <<END_SQL
-.timeout 2000
+# Update the added_at timestamp of Plex media items to the current date and time
+update_plex_media_timestamps() {
+    "${plexsql}" --sqlite "${plexdb}" <<END_SQL
+.timeout ${sql_timeout}
 UPDATE metadata_items SET added_at = DATETIME('now') WHERE DATETIME(added_at) > DATETIME('now');
 END_SQL
-docker start "${plexdockername}"
-#eof
+}
+
+# Start the Plex Docker container
+start_plex_container() {
+    docker start "${plexdockername}"
+}
+
+# Main function
+main() {
+    stop_plex_container
+    backup_plex_db
+    remove_sqlite_journals
+    update_plex_media_timestamps
+    start_plex_container
+}
+
+#________ EXECUTION
+# Call the main function
+main
--- plex_futures_n_stuckers.sh
+++ plex_futures_n_stuckers.sh
@@ -1,23 +1,73 @@
 #!/usr/bin/env bash
+# https://github.com/maximuskowalski/maxmisc/blob/master/plex_futures_n_stuckers.sh
 
+# This script corrects Plex items that have an incorrect added date in the future.
+# then resets the `added_at` dates of Plex items to their airdate or premiere date.
+
+# shellcheck disable=SC2154
+
+#________ DEV
+# set -x
+# set -Eeuo pipefail
+
+# IFS for safer handling of filenames and paths
+IFS=$'\n\t'
+
+#________ VARIABLES
+# Source the configuration file
+# shellcheck source=/dev/null
 source "$(dirname "$0")/maxmisc.conf"
 
-# plexdockername=plex
-# plexdbpath="/opt/plex/Library/Application Support/Plex Media Server/Plug-in Support/Databases/"
-# plexdb="com.plexapp.plugins.library.db"
-# plexsql="/opt/plexsql/Plex Media Server"
+#________ FUNCTIONS
+# Stop the Plex Docker container
+stop_plex_container() {
+    docker stop "${plexdockername}"
+}
 
-# today=$(date '+%Y_%d_%m__%H_%M_%S')
+# Backup the Plex database
+backup_plex_db() {
+    cd "${plexdbpath}" || return
+    cp "${plexdb}" "${plexdb}_${today}${backup_suffix}"
+}
 
-docker stop "${plexdockername}"
-cd "${plexdbpath}" || return
+# Remove the SQLite journal files from the Plex database directory
+remove_sqlite_journals() {
+    cd "${plexdbpath}" || return
+    rm -f com.plexapp.plugins.library.db-shm
+    rm -f com.plexapp.plugins.library.db-wal
+}
 
-cp "${plexdb}" "${plexdb}_${today}.bak"
-
-"${plexsql}" --sqlite "${plexdb}" <<END_SQL
-.timeout 2000
+# Reset the added_at timestamp of Plex media items to the current time
+reset_plex_media_timestamps() {
+    "${plexsql}" --sqlite "${plexdb}" <<END_SQL
+.timeout ${sql_timeout}
 UPDATE metadata_items SET added_at = DATETIME('now') WHERE DATETIME(added_at) > DATETIME('now');
+END_SQL
+}
+
+# Reset the added_at timestamp of Plex media items to their airdate or premiere date
+reset_plex_media_timestamps_to_airdate() {
+    "${plexsql}" --sqlite "${plexdb}" <<END_SQL
+.timeout ${sql_timeout}
 UPDATE metadata_items SET added_at = originally_available_at WHERE added_at <> originally_available_at AND originally_available_at IS NOT NULL;
 END_SQL
-docker start "${plexdockername}"
-#eof
+}
+
+# Start the Plex Docker container
+start_plex_container() {
+    docker start "${plexdockername}"
+}
+
+# Main function
+main() {
+    stop_plex_container
+    backup_plex_db
+    remove_sqlite_journals
+    reset_plex_media_timestamps
+    reset_plex_media_timestamps_to_airdate
+    start_plex_container
+}
+
+#________ EXECUTION
+# Call the main function
+main
